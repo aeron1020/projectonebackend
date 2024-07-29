a6367e47376267a34abb827a0d9996bde8e1e8df
aeron1020 <aeronpaduit20@gmail.com>
2024-07-29 12:53:41 +0800

    additional changes for the settings

--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settingsprod')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settingsprod')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
 
 application = get_wsgi_application()
