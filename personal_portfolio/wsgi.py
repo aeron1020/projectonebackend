2cd239f9b590c8711f7ce10def8501161d9f7c73
aeron1020 <aeronpaduit20@gmail.com>
2024-07-29 23:29:40 +0800

    sana

--- personal_portfolio/wsgi.py
+++ personal_portfolio/wsgi.py
@@ -12,6 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settingsprod')
-
-
 application = get_wsgi_application()
