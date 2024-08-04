1a607deaa559114a3d55a9d9a7c6e9075b58c255
aeron1020 <aeronpaduit20@gmail.com>
2024-08-04 22:50:09 +0800

    fixing personal_portfolio/manage.py

--- manage.py
+++ manage.py
@@ -1,41 +1,13 @@
-# #!/usr/bin/env python
-# """Django's command-line utility for administrative tasks."""
-# import os
-# import sys
-
-
-# def main():
-#     """Run administrative tasks."""
-
-#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settingsprod')
-    
-#     try:
-#         from django.core.management import execute_from_command_line
-#     except ImportError as exc:
-#         raise ImportError(
-#             "Couldn't import Django. Are you sure it's installed and "
-#             "available on your PYTHONPATH environment variable? Did you "
-#             "forget to activate a virtual environment?"
-#         ) from exc
-#     execute_from_command_line(sys.argv)
-
-
-# if __name__ == '__main__':
-#     main()
-
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settingsprod')
-    
-    # Add the project directory to the sys.path
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(current_path, 'personal_portfolio'))
-
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_portfolio.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -46,6 +18,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+
 if __name__ == '__main__':
     main()
-
